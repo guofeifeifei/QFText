db5412821c2f43fe7089c1c4d5da9bbc1559e8b3
GuoFeiFei <2545706530@qq.com>
2015-12-26 19:23:55 +0800

    Initial Commit

---
+++ textQF/textQF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textQF
+//
+//  Created by scjy on 15/12/26.
+//  Copyright (c) 2015年 郭飞飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textQF/textQF/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  textQF
+//
+//  Created by scjy on 15/12/26.
+//  Copyright (c) 2015年 郭飞飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
